4d3a5137410e39eb8666ea97b02bd36821934011
Fakile Razaq <razaqjimmie91@gmail.com>
2024-12-21 12:57:40 +0100

    Implemented Validation for Registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const User = require('../model/models');
+const bcrypt = require('bcrypt');
+const { registerValidation, loginValidation } = require('../validation');
+
 
 
 router.get('/register', (req, res) => {
@@ -7,10 +10,25 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', async(req, res) => {
+	//Register Credential Validation
+	const Validation = registerValidation(req.body);
+	if (Validation.error){
+		return res.status(400).send(Validation.error.details[0].message);
+	};
+
+	//Check if user already exists
+	const emailExist = await User.findOne({ email: req.body.email });
+	if(emailExist) return res.status(400).send('Email already exists');
+
+	 // Hash Password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+	//Create New User
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
-		password: req.body.password
+		password: hashedPassword
 	});
 	try{
 		await user.save();
@@ -22,6 +40,10 @@ router.post('/register', async(req, res) => {
 
 router.get('/login', (req,res) => {
 	res.render('login');
+});
+
+router.post('/login', (req, res) => {
+	
 })
 
 module.exports = router;
