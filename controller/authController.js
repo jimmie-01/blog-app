9728f5332ddefe5911563bc8b9de221f83a2a532
Fakile Razaq <razaqjimmie91@gmail.com>
2024-12-31 20:38:55 +0100

    User login error handling

--- controller/authController.js
+++ controller/authController.js
@@ -7,6 +7,16 @@ const handleErrors = (err) => {
   console.log(err.message, err.code);
   let errors = {email: '', password: ''};
 
+  //Email Error
+  if (err.message === 'Incorrect email') {
+    errors.email = 'That email is not registered';
+  }
+
+  //Password error
+  if (err.message === 'Incorrect password') {
+    errors.password = 'That password is incorrect';
+  }
+
   // duplicate user error handling
   if (err.code === 11000) {
     errors.email = 'that email is already registered';
@@ -54,6 +64,17 @@ module.exports.signup_post =  async (req, res) => {
     }
   };
   
-module.exports.login_post = (req, res) => {
-	res.send('log in');
+module.exports.login_post = async(req, res) => {
+	const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({ user: user._id });
+  }
+  catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
   };
